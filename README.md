297acb084c7eb3379c6339536191b3961c1153e2
archfeh <guyu2876@gmail.com>
2020-03-21 18:58:13 +0300

    update install.sh and reademe

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gnome-cn
-gnome中文翻译包(早于官方版)
+# GNOME-CN计划
+## gnome中文翻译包(早于官方版)
+### GNOME官方翻译人员凋敝，进度缓慢，于是将自用的翻译包发到本仓库供大家使用，也欢迎对翻译的修改和提议。
+
+## Version
+#### GNOME 3.36
+
+## 用法
+`sudo bash ./install.sh` 安装
+
+`sudo bash ./install.sh r1` 恢复
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+REPO_DIR=$(cd $(dirname $0) && pwd)
+SRC_DIR=${REPO_DIR}/src/
+
+ROOT_UID=0
+if [ "$UID" -eq "$ROOT_UID" ];then
+	#DEST_DIR="/usr/share/locale/zh_CN/LC_MESSAGES"
+	DEST_DIR="/home/archfeh/test/share/"
+else
+	echo "需要root或sudo权限"
+fi
+
+# Backup
+backup(){
+	echo "备份原文件中。。。以.bak结尾"
+	for filename in $(ls $SRC_DIR)
+	do
+		cd $DEST_DIR
+		cp $filename $filename.bak
+	done
+}
+
+# Install
+install(){
+	for filename in $(ls $SRC_DIR)
+
+	do 
+		echo "复制$filename到$DEST_DIR中"
+		cp $filename $DEST_DIR
+	done
+}
+
+# Revert
+revert(){
+	echo "正在恢复。。。"
+	for filename in $(ls $SRC_DIR)
+	do
+		cd $DEST_DIR
+		rm $filename
+		mv $filename.bak $filename
+	done
+}
+
+if [ "$1" != "r" ];then
+	backup
+	install
+fi
+
+if [ "$1" = "r" ];then
+	revert
+fi
+
+
